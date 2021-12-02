5c2a2ea4da552c469a57edc30dcb9e7165472b78
batuhan <batuhan@umich.edu>
2021-12-01 19:07:36 -0500

    Updated UMDA with objective funtion and new output

--- UMDA.cpp
+++ UMDA.cpp
@@ -2,10 +2,33 @@
 #include <random>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include <string>
 using namespace std;
 
-random_device r;     // only used once to initialise (seed) engine
-default_random_engine generator(r()); // random-number engine
+default_random_engine generator;
+vector<string> gcc_flags{"-fauto-inc-dec", "-fbranch-count-reg", "-fcombine-stack-adjustments", "-fcompare-elim", 
+						 "-fcprop-registers", "-fdce", "-fdefer-pop", "-fdelayed-branch", "-fdse", "-fforward-propagate",
+						 "-fguess-branch-probability", "-fif-conversion", "-fif-conversion2", "-finline-functions-called-once",
+						 "-fipa-profile", "-fipa-pure-const", "-fipa-reference", "-fipa-reference-addressable", "-fmerge-constants",
+						 "-fmove-loop-invariants", "-fomit-frame-pointer", "-freorder-blocks", "-fshrink-wrap", "-fshrink-wrap-separate",
+						 "-fsplit-wide-types", "-fssa-backprop", "-fssa-phiopt", "-ftree-bit-ccp", "-ftree-ccp", "-ftree-ch", "-ftree-coalesce-vars",
+						 "-ftree-copy-prop", "-ftree-dce", "-ftree-dominator-opts", "-ftree-dse", "-ftree-forwprop", "-ftree-fre",
+						 "-ftree-phiprop", "-ftree-pta", "-ftree-scev-cprop", "-ftree-sink", "-ftree-slsr", "-ftree-sra",
+						 "-ftree-ter", "-funit-at-a-time", "-falign-functions", "-falign-jumps", "-falign-labels", "-falign-loops",
+						 "-fcaller-saves", "-fcode-hoisting", "-fcrossjumping", "-fcse-follow-jumps", "-fcse-skip-blocks", 
+						 "-fdelete-null-pointer-checks", "-fdevirtualize", "-fdevirtualize-speculatively", "-fexpensive-optimizations",
+						 "-fgcse", "-fgcse-lm", "-fhoist-adjacent-loads", "-finline-small-functions", "-findirect-inlining", "-fipa-bit-cp",
+						 "-fipa-cp", "-fipa-icf", "-fipa-ra", "-fipa-sra", "-fipa-vrp", "-fisolate-erroneous-paths-dereference", 
+						 "-flra-remat", "-foptimize-sibling-calls", "-foptimize-strlen", "-fpartial-inlining", "-fpeephole2",
+						 "-freorder-blocks-algorithm=stc", "-freorder-blocks-and-partition", "-freorder-functions", "-frerun-cse-after-loop",
+						 "-fschedule-insns", "-fschedule-insns2", "-fsched-interblock", "-fsched-spec", "-fstore-merging", "-fstrict-aliasing",
+						 "-fthread-jumps", "-ftree-builtin-call-dce", "-ftree-pre", "-ftree-switch-conversion", "-ftree-tail-merge",
+						 "-ftree-vrp", "-fgcse-after-reload", "-finline-functions", "-fipa-cp-clone", "-floop-interchange", "-floop-unroll-and-jam",
+						 "-fpeel-loops", "-fpredictive-commoning", "-fsplit-paths", "-ftree-loop-distribute-patterns", "-ftree-loop-distribution",
+						 "-ftree-loop-vectorize", "-ftree-partial-pre", "-ftree-slp-vectorize", "-funswitch-loops", "-fvect-cost-model",
+						 "-fversion-loops-for-strides"};
+
 
 struct result {
     vector<vector<int> > min_flags;
@@ -13,20 +36,59 @@ struct result {
 };
 
 void print_flags(vector<int> flags){
-    cout << "[";
     for (size_t i = 0; i < flags.size() - 1; i++){
-        cout << flags[i] << ", ";
+        cout << flags[i] << ",";
     }
-    cout << flags[flags.size() - 1] << "]" << endl;
+    cout << flags[flags.size() - 1] << endl;
 }
 
-float objective(vector<int> flags){
-    int sum = 0;
-    for (size_t i = 0; i< flags.size(); i++){
-        sum += flags[i];
+void print_min(vector<float> min_runtime, vector<vector<int>> min_flags){
+    float time = min_runtime[0];
+    size_t gen = 0;
+    vector<int> flags = min_flags[0];
+    for (size_t i = 0; i < min_runtime.size(); i++){
+        if (time > min_runtime[i]){
+            time = min_runtime[i];
+            flags = min_flags[i];
+            gen = i;
+        }
     }
-    return float(sum);
-};
+    cout << "best" << ";time:" << time << ";gen:" << gen << ";flags:";
+    print_flags(flags);
+}
+
+float objective(vector<int> flags, string compile_files, string filename){
+	//compile_files: files to compile by gcc
+	//sample compile_files: "basicmath/basicmath_large.c basicmath/rad2deg.c basicmath/cubic.c basicmath/isqrt.c "
+	//filename: compiled filename
+	//sample filename: "basicmath_large"
+	string compile_flags;
+	for(size_t i = 0; i < flags.size(); i++){
+		if(flags[i] == 1){
+			compile_flags += gcc_flags[i];
+			compile_flags += " ";
+		}
+	}
+    string command1 = "gcc " + compile_flags + compile_files + "-o " + filename + " -lm";
+	string command2 = "bash -c '(TIMEFORMAT='%3R'; time ./" + filename + " > output.txt) &> time.txt'";
+
+    system(command1.c_str());
+    system(command2.c_str());
+
+	ifstream myfile("time.txt");
+	string line;
+	float seconds;
+	if (myfile.is_open())
+	{
+		getline(myfile, line);
+		seconds = stof(line);
+		myfile.close();
+	}
+	else{
+		cout << "Unable to open file";
+	}
+	return seconds;
+}
 
 vector<vector<int> > sampling(vector<float> probability, int n_pop){
     //generate population
@@ -97,7 +159,7 @@ vector<float> cal_prob(vector<vector<int> > population){
     return prob;
 }
 
-result UMDA(float (*fun)(vector<int>), int n_flags, int n_gen, int n_pop, float r_mut, float r_sel){
+result UMDA(float (*fun)(vector<int>, string, string), string compile_files, string filename, int n_flags, int n_gen, int n_pop, float r_mut, float r_sel){
     //initialize population; initial probability is 0.5 for each flag
     vector<float> prob(n_flags, 0.5);
     vector<vector<int> > population = sampling(prob, n_pop);
@@ -108,13 +170,13 @@ result UMDA(float (*fun)(vector<int>), int n_flags, int n_gen, int n_pop, float 
     vector<vector<int> > min_flags(n_gen, vector<int> (n_flags,0));
 
     for(size_t i = 0; i < n_gen; i++){
-        min_runtime[i] = (*fun)(population[0]);
+        min_runtime[i] = (*fun)(population[0], compile_files, filename);
         min_flags[i] = population[0];
 
         vector<pair<float,int> > runtimes(n_pop, make_pair(0.0,0));
         //calculate runtime for each individual in the population
         for (int j = 0; j < n_pop; j++){
-            float score = (*fun)(population[j]);
+            float score = (*fun)(population[j], compile_files, filename);
             runtimes[j] = make_pair(score, j);
         }
 
@@ -131,11 +193,14 @@ result UMDA(float (*fun)(vector<int>), int n_flags, int n_gen, int n_pop, float 
             min_runtime[i] = cur_min_runtime;
             min_flags[i] = cur_min_flag;
         }
+        cout << "gen:" << i << ";" << "time:" << min_runtime[i] << endl;
 
         prob = cal_prob(selected_population);
         population = sampling(prob, n_pop);
         mutation(population, r_mut);
     }
+
+    print_min(min_runtime, min_flags);
     
     result test;
     test.min_flags = min_flags;
@@ -146,10 +211,12 @@ result UMDA(float (*fun)(vector<int>), int n_flags, int n_gen, int n_pop, float 
 }
 
 int main(){
+    // define the algorithm used
+    string alg = "UMDA";
     // define the number of generations
     int n_gen = 50;
     // define the number of flags
-    int n_flags = 58;
+    int n_flags = 107;
     // define the population size
     int n_pop = 100;
     // mutation rate
@@ -158,10 +225,24 @@ int main(){
     float r_sel = 0.5;
     // perform the UMDA
     int execution_time = 30;
-    vector<result> results_each_generation;
 
+    vector<result> results_each_generation;
+	string filename("basicmath_small");
+	string compile_files("experiment/basicmath/basicmath_small.c experiment/basicmath/rad2deg.c experiment/basicmath/cubic.c experiment/basicmath/isqrt.c ");
+
+    cout << "parameters" << ";"; 
+    cout << "alg:" << alg << ";";
+    cout << "n_exe:" << execution_time << ";";
+    cout << "n_gen:" << n_gen << ";";
+    cout << "n_flags:" << n_flags << ";";
+    cout << "n_pop:" << n_pop << ";";
+    cout << "r_mut:" << r_mut << ";";
+    cout << "r_sel:" << r_sel << ";";
+    cout << "filename:" << filename << endl;
     for(size_t i = 0; i < execution_time; i++){
-        result first = UMDA(objective, n_flags, n_gen, n_pop, r_mut, r_sel);
+		cout << "exe:" << i << endl;
+        result first = UMDA(objective, compile_files, filename, n_flags, n_gen, n_pop, r_mut, r_sel);
+
         results_each_generation.push_back(first);
     }
 
@@ -183,6 +264,7 @@ int main(){
     for (size_t i = 0; i < n_gen; i++){
         cout << efficiency_gain[i] << ", ";
     }
+	cout << endl;
 
     return 0;
 }
