c409e72ca83ed621ca9d01dc9f5a7eb48ff91917
KevinRSiu <kevin.rodriguez.siu@ucsp.edu.pe>
2021-12-02 18:28:36 -0500

    Experiment.cpp now has options for small and large inputs.

--- experiment.cpp
+++ experiment.cpp
@@ -3,52 +3,76 @@
 #include <string>
 #include <stdlib.h> // for strtol()
 #include <errno.h>  // for errno
+#include <sstream>  
 
 
 using namespace std;
 
 //Specify experiments so they can automatically grab the routes and the filename
 const string guide = "Usage:\n\
-                \t experiment [dir] [exp_num] [algorithm] \n\
+                \t experiment [dir] [exp_num] [small/large] [algorithm] \n\
                 [dir]: directory containing the experiment files. Default is \"/experiment\"\n\
-                [exp_num]: 0 - basicmath_small , 1 - basicmath_large, 2 - bitcount, 3 - qsort_small, 4 - qsort_large, 5 - susan\n\
+                [exp_num]: 0 - basicmath , 1 - bitcount, 2 - qsort, 3 - susan\n\
+                [small/large]: Will the algorithm use the small (-s or --small) or large (-l or --large) input?\n\
                 [algorithm]: 0 - GA, 1 - UMDA, 2 - TreeEDA\n";
                 
                 
 //Data needed for every experiment
 const vector<string> experiment_filename = {"basicmath_small", 
                                             "basicmath_large",
-                                            "bitcnts",
+                                            "bitcnts_small",
+                                            "bitcnts_large",
                                             "qsort_small",
                                             "qsort_large",
-                                            "susan"};
+                                            "susan_small",
+                                            "susan_large"};
 
 const vector<string> experiment_compile_files = {"basicmath/basicmath_small.c   basicmath/rad2deg.c  basicmath/cubic.c   basicmath/isqrt.c",
                                                  "basicmath/basicmath_large.c   basicmath/rad2deg.c  basicmath/cubic.c   basicmath/isqrt.c",
                                                  "bitcount/bitcnt_1.c bitcount/bitcnt_2.c bitcount/bitcnt_3.c bitcount/bitcnt_4.c bitcount/bitcnts.c bitcount/bitfiles.c bitcount/bitstrng.c bitcount/bstr_i.c",
+                                                 "bitcount/bitcnt_1.c bitcount/bitcnt_2.c bitcount/bitcnt_3.c bitcount/bitcnt_4.c bitcount/bitcnts.c bitcount/bitfiles.c bitcount/bitstrng.c bitcount/bstr_i.c",
                                                  "qsort/qsort_small.c",
                                                  "qsort/qsort_large.c",
+                                                 "susan/susan.c",
                                                  "susan/susan.c"}; 
 
 const vector<string> ev_algorithms = {"GA", "UMDA", "TreeEDA"};
+
+string updated_compile_files(string route, int exp_num){
+    
+    string og_compile_files = experiment_compile_files[exp_num];
+    string result = "";
+
+    istringstream iss(og_compile_files);
+    string temp; 
+    while(iss >> temp){
+        result += route;
+        result += "/";
+        result += temp;
+        result += " ";
+    }
+
+    return result; 
+}
 
 int main(int argc, char **argv){
 
     cout << "EECS583-FA21 Experiment!" << endl;
     
-    if(argc != 4){
+    if(argc != 5){
         cout << "Incorrect arguments!" << endl;
         cout << guide << endl;
         return 1;
     }
 
     string experiment_route = argv[1];
+    string input_size = argv[3];
 
     char* p;
     char* q;
     errno = 0; // not 'int errno', because the '#include' already defined it
     long arg1 = strtol(argv[2], &p, 10);
-    long arg2 = strtol(argv[3], &q, 10);
+    long arg2 = strtol(argv[4], &q, 10);
     if (*p != '\0' || *q != '\0' || errno != 0) {
         cout << "Argument error! Only select a number for the experiments and algorithms" << endl;
         return 2; 
@@ -58,10 +82,24 @@ int main(int argc, char **argv){
         cout << "Argument error! Experiment or Algorithm number doesn't exists" << endl;
         return 3;
     }
-    int experiment_ind = arg1;
+
+    int uses_large_input = -1;
+
+    if(input_size == "-l" || input_size == "--large"){
+        uses_large_input = 1;
+    }else if(input_size == "-s"){
+        uses_large_input = 0;
+    }else{
+        cout << "Argument error! Size of experiment invalid" << endl;
+        return 3;   
+    }
+
+    int experiment_ind = 2*arg1 + uses_large_input;
     int algorithm_ind = arg2;
 
     // IMPORT ALGORITHMS NOW 
+
+    cout << updated_compile_files(experiment_route, experiment_ind) << endl;
 
     return 0;
 
