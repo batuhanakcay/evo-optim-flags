aaad796f0f9721dc4d0c13063c8d30629c5109bb
batuhan <batuhan@umich.edu>
2021-12-01 20:06:32 -0500

    Updated ga with objective function

--- ga.cpp
+++ ga.cpp
@@ -4,34 +4,96 @@
 #include <limits>
 #include <tuple>
 #include <algorithm>
+#include <string>
+#include <fstream>
 
 
 using namespace std;
 
-random_device r;     // only used once to initialise (seed) engine
-default_random_engine e1(r());    // random-number engine
+default_random_engine e1;    // random-number engine
+
+vector<string> gcc_flags{"-fauto-inc-dec", "-fbranch-count-reg", "-fcombine-stack-adjustments", "-fcompare-elim", 
+						 "-fcprop-registers", "-fdce", "-fdefer-pop", "-fdelayed-branch", "-fdse", "-fforward-propagate",
+						 "-fguess-branch-probability", "-fif-conversion", "-fif-conversion2", "-finline-functions-called-once",
+						 "-fipa-profile", "-fipa-pure-const", "-fipa-reference", "-fipa-reference-addressable", "-fmerge-constants",
+						 "-fmove-loop-invariants", "-fomit-frame-pointer", "-freorder-blocks", "-fshrink-wrap", "-fshrink-wrap-separate",
+						 "-fsplit-wide-types", "-fssa-backprop", "-fssa-phiopt", "-ftree-bit-ccp", "-ftree-ccp", "-ftree-ch", "-ftree-coalesce-vars",
+						 "-ftree-copy-prop", "-ftree-dce", "-ftree-dominator-opts", "-ftree-dse", "-ftree-forwprop", "-ftree-fre",
+						 "-ftree-phiprop", "-ftree-pta", "-ftree-scev-cprop", "-ftree-sink", "-ftree-slsr", "-ftree-sra",
+						 "-ftree-ter", "-funit-at-a-time", "-falign-functions", "-falign-jumps", "-falign-labels", "-falign-loops",
+						 "-fcaller-saves", "-fcode-hoisting", "-fcrossjumping", "-fcse-follow-jumps", "-fcse-skip-blocks", 
+						 "-fdelete-null-pointer-checks", "-fdevirtualize", "-fdevirtualize-speculatively", "-fexpensive-optimizations",
+						 "-fgcse", "-fgcse-lm", "-fhoist-adjacent-loads", "-finline-small-functions", "-findirect-inlining", "-fipa-bit-cp",
+						 "-fipa-cp", "-fipa-icf", "-fipa-ra", "-fipa-sra", "-fipa-vrp", "-fisolate-erroneous-paths-dereference", 
+						 "-flra-remat", "-foptimize-sibling-calls", "-foptimize-strlen", "-fpartial-inlining", "-fpeephole2",
+						 "-freorder-blocks-algorithm=stc", "-freorder-blocks-and-partition", "-freorder-functions", "-frerun-cse-after-loop",
+						 "-fschedule-insns", "-fschedule-insns2", "-fsched-interblock", "-fsched-spec", "-fstore-merging", "-fstrict-aliasing",
+						 "-fthread-jumps", "-ftree-builtin-call-dce", "-ftree-pre", "-ftree-switch-conversion", "-ftree-tail-merge",
+						 "-ftree-vrp", "-fgcse-after-reload", "-finline-functions", "-fipa-cp-clone", "-floop-interchange", "-floop-unroll-and-jam",
+						 "-fpeel-loops", "-fpredictive-commoning", "-fsplit-paths", "-ftree-loop-distribute-patterns", "-ftree-loop-distribution",
+						 "-ftree-loop-vectorize", "-ftree-partial-pre", "-ftree-slp-vectorize", "-funswitch-loops", "-fvect-cost-model",
+						 "-fversion-loops-for-strides"};
 
 struct result {
-    vector<vector<int>> min_flags;
+    vector<vector<int> > min_flags;
     vector<float> min_runtimes;
 };
 
-void print_flags(vector<int> flags){
-    cout << "[";
+void print_flags(const vector<int> &flags){
     for (size_t i = 0; i < flags.size() - 1; i++){
-        cout << flags[i] << ", "; 
+        cout << flags[i] << ",";
     }
-    cout << flags[flags.size() - 1] << "]" << endl;
+    cout << flags[flags.size() - 1] << endl;
+}
+
+void print_min(const vector<float> &min_runtime, const vector<vector<int>> &min_flags){
+    float time = min_runtime[0];
+    size_t gen = 0;
+    vector<int> flags = min_flags[0];
+    for (size_t i = 0; i < min_runtime.size(); i++){
+        if (time > min_runtime[i]){
+            time = min_runtime[i];
+            flags = min_flags[i];
+            gen = i;
+        }
+    }
+    cout << "best" << ";time:" << time << ";gen:" << gen << ";flags:";
+    print_flags(flags);
 }
 
 
-float objective(vector<int> flags){
-    int sum = 0;
-    for (size_t i = 0; i< flags.size(); i++){
-        sum += flags[i];
-    }
-    return float(sum);
-};
+float objective(const vector<int> &flags, string compile_files, string filename){
+	//compile_files: files to compile by gcc
+	//sample compile_files: "basicmath/basicmath_large.c basicmath/rad2deg.c basicmath/cubic.c basicmath/isqrt.c "
+	//filename: compiled filename
+	//sample filename: "basicmath_large"
+	string compile_flags;
+	for(size_t i = 0; i < flags.size(); i++){
+		if(flags[i] == 1){
+			compile_flags += gcc_flags[i];
+			compile_flags += " ";
+		}
+	}
+    string command1 = "gcc " + compile_flags + compile_files + "-o " + filename + " -lm";
+	string command2 = "bash -c '(TIMEFORMAT='%3R'; time ./" + filename + " > output.txt) &> time.txt'";
+
+    system(command1.c_str());
+    system(command2.c_str());
+
+	ifstream myfile("time.txt");
+	string line;
+	float seconds;
+	if (myfile.is_open())
+	{
+		getline(myfile, line);
+		seconds = stof(line);
+		myfile.close();
+	}
+	else{
+		cout << "Unable to open file";
+	}
+	return seconds;
+}
 
 vector<int> selection(vector<tuple<float,int>> sorted_runtimes, float r_sel){
     int n_parents = int(r_sel * sorted_runtimes.size());
@@ -76,11 +138,7 @@ void mutation(vector<vector<int>> & children, float r_mut){
     }
 }
 
-result genetic_algorithm(float (*fun)(vector<int>), int n_flags, int n_gen, int n_pop, float r_mut, float r_sel){
-    result test;
-    vector<vector<int>> min_flags(n_gen + 1);
-    vector<float> min_runtimes(n_gen + 1);
-
+result genetic_algorithm(float (*fun)(const vector<int> &, string, string), string compile_files, string filename, int n_flags, int n_gen, int n_pop, float r_mut, float r_sel){
     uniform_int_distribution<int> uniform_dist(0, 1);
     vector<vector<int>> population(n_pop, vector<int> (n_flags));
     for (int i = 0; i < n_pop; i++){
@@ -88,31 +146,24 @@ result genetic_algorithm(float (*fun)(vector<int>), int n_flags, int n_gen, int 
             population[i][j] = uniform_dist(e1);
         }
     }
-    float min_runtime = (*fun)(population[0]);
-    int min_index = 0;
+
+    //container for best performance in each generation
+    vector<float> min_runtime(n_gen);
+    vector<vector<int> > min_flags(n_gen, vector<int> (n_flags));
+
     for (int i = 0; i < n_gen; i++){
         vector<tuple<float,int>> runtimes(n_pop);
         for (int j = 0; j < n_pop; j++){
-            float score = (*fun)(population[j]);
+            float score = (*fun)(population[j], compile_files, filename);
             runtimes[j] = make_tuple(score, j);
         }
         sort(runtimes.begin(), runtimes.end());
-        float cur_gen_min_runtime = get<0>(runtimes[0]);
-        int cur_gen_min_index = get<1>(runtimes[0]);
-        if (cur_gen_min_runtime < min_runtime){
-            min_runtime = cur_gen_min_runtime;
-            min_index = cur_gen_min_index;
-            /*
-            cout << "Generation " << i << endl;
-            cout << "New min runtime: " <<  min_runtime << endl;
-            cout <<  "New min flags: [";
-            for (int k = 0; k < n_flags - 1; k++){
-                cout <<  population[min_index][k] << ", ";
-            }
-            cout <<  population[min_index][n_flags - 1] << "]" << endl;
-            cout << endl;
-            */
-        }
+
+        min_runtime[i] = get<0>(runtimes[0]);
+        min_flags[i] = population[get<1>(runtimes[0])];
+
+        cout << "gen:" << i << ";" << "time:" << min_runtime[i] << endl;
+
         vector<int> parents_index = selection(runtimes, r_sel);
         vector<vector<int>> new_generation;
         for (size_t j = 0 ; j < parents_index.size(); j = j + 2){
@@ -131,74 +182,73 @@ result genetic_algorithm(float (*fun)(vector<int>), int n_flags, int n_gen, int 
         mutation(crossover_children, r_mut);
         new_generation.push_back(crossover_children[0]);
         new_generation.push_back(crossover_children[1]);
-    min_flags[i] = population[min_index];
-    min_runtimes[i] = min_runtime;
-    population = new_generation;
+        population = new_generation;
     }
-    vector<tuple<float,int>> runtimes(n_pop);
-    for (int j = 0; j < n_pop; j++){
-        float score = (*fun)(population[j]);
-        runtimes[j] = make_tuple(score, j);
-    }
-    sort(runtimes.begin(), runtimes.end());
-    min_runtime = get<0>(runtimes[0]);
-    min_index = get<1>(runtimes[0]);
-    min_flags[n_gen] = population[min_index];
-    min_runtimes[n_gen] = min_runtime;
+
+    print_min(min_runtime, min_flags);
+
+    result test;
     test.min_flags = min_flags;
-    test.min_runtimes = min_runtimes;
+    test.min_runtimes = min_runtime;
+
     return test;
 }
 
 int main(){
+    // define the algorithm used
+    string alg = "GA";
     // define the number of generations
-    int n_gen = 50;
+    int n_gen = 2;
     // define the number of flags
-    int n_flags = 58;
+    int n_flags = 107;
     // define the population size
     int n_pop = 100;
-    // One point crossover
     // mutation rate
     float r_mut = 1.0 / float(n_flags);
     // trunctation rate
     float r_sel = 0.5;
-    // perform the GA
-    int execution_time = 30;
-    vector<result> results_each_generation(execution_time);
+    // perform the UMDA
+    int execution_time = 2;
 
+    vector<result> results_each_generation(execution_time);
+	string filename("basicmath_small");
+	string compile_files("experiment/basicmath/basicmath_small.c experiment/basicmath/rad2deg.c experiment/basicmath/cubic.c experiment/basicmath/isqrt.c ");
+
+    cout << "parameters" << ";"; 
+    cout << "alg:" << alg << ";";
+    cout << "n_exe:" << execution_time << ";";
+    cout << "n_gen:" << n_gen << ";";
+    cout << "n_flags:" << n_flags << ";";
+    cout << "n_pop:" << n_pop << ";";
+    cout << "r_mut:" << r_mut << ";";
+    cout << "r_sel:" << r_sel << ";";
+    cout << "filename:" << filename << endl;
     for(size_t i = 0; i < execution_time; i++){
-        cout << "Execution " << i << endl;
-        // cout << endl;
-        // perform the genetic algorithm search
-        result test = genetic_algorithm(objective, n_flags, n_gen, n_pop, r_mut, r_sel);
-        results_each_generation[i] = test;
-        /*
-        for (size_t j = 0; j < n_gen; j++){
-            cout << "Generation: " << j << endl;
-            print_flags(results_each_generation[i].min_flags[j]);
-            cout << "Runtime: " << results_each_generation[i].min_runtimes[j] << endl;
-        }
-        cout << endl;
-        */
+		cout << "exe:" << i << endl;
+        result first = genetic_algorithm(objective, compile_files, filename, n_flags, n_gen, n_pop, r_mut, r_sel);
+
+        results_each_generation[i] = first;
     }
 
-    //calculate average min runtime for each generation
-    vector<float> avg_min_runtime(n_gen + 1,0);
-    for(size_t i = 0; i < n_gen + 1; i++){
+    //calculate efficiency gain
+    vector<float> avg_min_runtime(n_gen,0);
+    for(size_t i = 0; i < n_gen; i++){
         float sum = 0;
         for(size_t j = 0; j < execution_time; j++){
             sum += results_each_generation[j].min_runtimes[i];
         }
         avg_min_runtime[i] = sum/execution_time;
     }
-
-    cout << endl;
-    cout << "After running algorithm " << execution_time << " times the average runtime for generations are:" << endl;
-    cout << endl;
-    for (size_t i = 0; i < n_gen + 1; i++){
-        cout << "Generation: " << i << endl;
-        cout << "Average Runtime: " << avg_min_runtime[i] << endl;
+    vector<float> efficiency_gain(n_gen,0);
+    for(size_t i = 1; i < n_gen; i++){
+        efficiency_gain[i] = (avg_min_runtime[0]-avg_min_runtime[i])/avg_min_runtime[0];
     }
+    
+    cout << "Efficiency Gain" << endl;
+    for (size_t i = 0; i < n_gen; i++){
+        cout << efficiency_gain[i] << ", ";
+    }
+	cout << endl;
 
     return 0;
 }
